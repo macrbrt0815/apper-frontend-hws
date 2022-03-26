eea24f1949cdd24514343e4623202d91ff71e011
Mac Robert Cabote <macrobert.cabote@GFI05286.local>
2022-03-26 21:37:16 +0800

    HW1-6

--- HW1/HW1-1.js
+++ HW1/HW1-1.js
@@ -1,14 +1,14 @@
 function transform (integers){
     const squaredIntegers = []
-    integers.forEach((item, integers) => {
-        if(typeof item === 'number'){ //validate id item is of type number
-            squaredIntegers.push(item*item)
+    integers.forEach((int, integers) => {
+        if(typeof int === 'number'){ //validate id item is of type number
+            squaredIntegers.push(int*int)
         }
     })
     const sortedArray = new Uint32Array(squaredIntegers).sort()
     return sortedArray
 }
 
-const integers = [20,30,1,2,3,4,5, 'one'] //enter array of integers
+const integers = [20,30,1,2,3,4,5] //enter array of integers
 const squaredIntegers = transform(integers)
 console.log(squaredIntegers)
